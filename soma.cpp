30ac43cba7b75b82f2792c7714e12902eeef3c6a
Mírian F <contato.mirianfs@gmail.com>
2017-08-24 21:41:33 -0300

    Create soma.cpp

---
+++ soma.cpp
@@ -0,0 +1,18 @@
+/* -----------------------------------------
+	SPOJ.com | Problem SOMA 
+  ----------------------------------------- */
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n, i, x = 0, sum = 0;
+	
+	cin >> n;
+	for(i = 0; i < n; i++){
+		cin >> x;
+		sum = sum + x;
+	}
+	cout << sum;
+	return 0;
+}
