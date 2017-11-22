27777370838893ddccae8c502e821d5f96434230
Mírian F <contato.mirianfs@gmail.com>
2017-11-22 00:01:14 -0200

    Create uri-1372.cpp

---
+++ uri-1372.cpp
@@ -0,0 +1,33 @@
+/* ----------------------------------------------
+Username: mirianfs 
+---------------------------------------------- */
+
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+
+#define MAX 100001
+using namespace std;
+
+int dp[MAX], col[MAX], row[MAX], r, c;
+
+int solve(int i, int n, int *a){
+	if(i >= n) return 0;
+	if(dp[i]) return dp[i];
+	return dp[i] = max(a[i]+solve(i+2,n,a), solve(i+1,n,a));
+}
+
+int main(){
+	int i, j;
+	while(scanf("%d%d", &r, &c)==2 && (r || c)){
+		for(i=0; i<r; i++){
+			for(j=0; j<c; j++) 
+				cin >> row[j];
+			fill(dp, dp+c, 0);
+			col[i] = solve(0, c, row);
+		}
+		fill(dp, dp+r, 0);
+		cout << solve(0,r,col) << endl;
+	}
+	return 0;
+}
